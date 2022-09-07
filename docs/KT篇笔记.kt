5e7bf700d156a6f17810a22cb4121ca4862fc6b4
zhongzhen <hongxiangzhang.zhx@alibaba-inc.com>
2022-09-07 15:02:32 +0800

    samples

---
+++ docs/KT篇笔记.kt
@@ -0,0 +1,47 @@
+package docs
+
+object S1 {
+    /**
+     * 1，函数写法
+     */
+    fun s1Sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun s1Sum1(a: Int, b: Int) = a + b
+    fun s1SumUnit(a: Int): Unit = Unit
+
+    /**
+     * 2,遍历
+     */
+    fun run1() {
+        val a = 1..5
+        print("\nout $a")
+        val input = 3
+        when (input) {
+            in a, is Int -> print("\ninput in int")
+            is Int -> ""
+            else -> {}
+        }
+        val maps = mapOf<String, Int>("1" to 1, "2" to 2)
+        for ((k, value) in maps) {
+            print("\n$k -> $value")
+        }
+    }
+
+    /**
+     * 3，基本类型
+     */
+    fun run2() {
+        val string = """
+            
+            DO run this Action
+            ${'$'}WITH ME
+        """.trimIndent()
+        print(string)
+
+        val nullString:String? = "";
+        nullString?.length
+        print("\n${nullString}")
+    }
+}
--- samples/JavaSamples.kt
+++ samples/JavaSamples.kt
@@ -1,5 +1,17 @@
 package samples
 
-fun main(){
+import docs.S1
+import java.awt.Rectangle
 
+fun main() {
+    val user = User("jack",20)
+    val (hack,age) = user
+    val copy = user.copy(age = 10)
+    print(copy.toString())
+    print("\nout ${S1.s1SumUnit(1)}")
+
+    S1.run1()
+    S1.run2()
 }
+
+data class User(val name: String = "", var age: Int = 0)
